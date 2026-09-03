821c79b1514bc0a844e6461ea568733ab7cb028e
weijiun <u8039500@hotmail.com>
2026-09-03 15:25:10 +0800

    Add OrdersVO

---
+++ src/main/java/com/fruitute/orders/OrdersVO.java
@@ -0,0 +1,19 @@
+package com.fruitute.orders;
+
+public class OrdersVO {
+	Integer ordersId;
+	String orderName;
+	public Integer getOrdersId() {
+		return ordersId;
+	}
+	public void setOrdersId(Integer ordersId) {
+		this.ordersId = ordersId;
+	}
+	public String getOrderName() {
+		return orderName;
+	}
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+
+}
